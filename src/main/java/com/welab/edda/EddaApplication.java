89c00cd9cff1d9baab20ec995afbd98d572590ca
martin <1004185602@qq.com>
2020-06-08 22:18:21 +0800

    [TPRG-19] feature: new edda project

--- src/main/java/com/welab/edda/EddaApplication.java
+++ src/main/java/com/welab/edda/EddaApplication.java
@@ -3,6 +3,9 @@ package com.welab.edda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * will add api swagger later
+ */
 @SpringBootApplication
 public class EddaApplication {
 
