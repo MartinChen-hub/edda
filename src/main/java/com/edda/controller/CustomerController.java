8c16ba430c5a92ff1372e05df7a161d525c98da4
markchen20190628 <1004185602@qq.com>
2020-06-29 14:48:53 +0800

    customer proxy

--- src/main/java/com/edda/controller/CustomerController.java
+++ src/main/java/com/edda/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.edda.controller;
 
+import com.edda.proxy.CustomerProxy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,9 +9,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
-@RequestMapping("v1")
+@RequestMapping("/customers")
 public class CustomerController {
+
+    private CustomerProxy customerProxy;
+
+    public CustomerController(CustomerProxy customerProxy){
+        this.customerProxy = customerProxy;
+    }
 
 //    @GetMapping(path = "/customer")
 //    public ResponseEntity<String> customer(@RequestParam("id") String id) {
@@ -41,12 +50,18 @@ public class CustomerController {
 //        return new ResponseEntity(map, null, HttpStatus.OK);
 //    }
 
-    @PostMapping(path = {"/customer/{name}"})
-    public ResponseEntity postCustomer(@PathVariable("name") String name){
-        System.out.println("post==>"+name);
-        Map<String,String> map = new HashMap<String,String>();
-        map.put("result","success");
-        map.put("info","成功了！");
-        return new ResponseEntity(map, null, HttpStatus.OK);
+    @GetMapping(path="/{customer}")
+    public ResponseEntity<Map<String, String>> getCustomer(@PathVariable("customer") String customer){
+        System.out.println("get a my controller request"+customer);
+        Map<String, String> map = new HashMap<>();
+        map.put("1", customer);
+        return new ResponseEntity(map,null, HttpStatus.OK);
+    }
+
+    @GetMapping(path = {"/{customerId}/name"})
+    public ResponseEntity getCustomerName(@PathVariable("customerId") String customerId){
+        log.info("receive a request to edda controller to get customer name {}", customerId);
+        String customerName = customerProxy.getCustomerName(customerId);
+        return new ResponseEntity(customerName, null, HttpStatus.OK);
     }
 }
